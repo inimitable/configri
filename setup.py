8b31f003d8f7f94a7d75172c758e1b0cd8c6da65
inimitable <inimitable@users.noreply.github.com>
2019-11-18 11:12:44 -0500

    Change license in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['configri', 'configri.main', 'configri.test', 'configri.utils', 'configri.backends', 'configri.backends.ini', 'configri.backends.base',
               'configri.backends.json', 'configri.backends.toml'],
     url='https://github.com/inimitable/configri',
-    license='CC BY-SA',
+    license='GNU AFFERO GENERAL PUBLIC LICENSE V3',
     author='Rob Ewing',
     author_email='inimitable@users.noreply.github.com',
     description='A simple, extensible, cross-platform config manager framework.'
