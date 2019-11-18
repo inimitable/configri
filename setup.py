843952380cf01f2f1af88f09dbac7c47fca5f18d
inimitable <anonymous@none.org>
2019-11-18 11:25:49 -0500

    Restructuring tests && adding pytest.ini

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='Configri',
     version='0.2',
-    packages=['configri', 'configri.main', 'configri.test', 'configri.utils', 'configri.backends', 'configri.backends.ini', 'configri.backends.base',
+    packages=['configri', 'configri.main', 'configri.utils', 'configri.backends', 'configri.backends.ini', 'configri.backends.base',
               'configri.backends.json', 'configri.backends.toml'],
     url='https://github.com/inimitable/configri',
     license='GNU AFFERO GENERAL PUBLIC LICENSE V3',
